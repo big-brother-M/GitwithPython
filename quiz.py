1ca14baef8f3d4691ff015e27e9f80483c640166
big-brother <big-brother@kakao.com>
2026-04-02 15:23:19 +0900

    Feat : 기본 퀴즈 데이터 5개 추가

--- quiz.py
+++ quiz.py
@@ -15,3 +15,32 @@ class Quiz:
     def check_answer(self, user_answer):
         return self.answer == user_answer
     
+    def get_default_quizzers():
+        quizzes = [
+            Quiz(  
+                queestion = "PYTHON의 창시자는 누구인가요?",
+                choices = ["빌게이츠", "스티브 잡스", "귀도 반 로섬", "이재용"],
+                answer = 3
+            ),
+            Quiz(
+                queestion = "다음 중 PYTHON의 기본 자료형이 아닌것은?",
+                choices = ["int", "str", "list", "array"],
+                answer = 4
+            ),
+            Quiz(
+                queestion = "Git에서 변경사항을 확정짓는 명령어는?",
+                choices = ["git add", "git commit", "git push", "git pull"],
+                answer = 2
+            ),
+            Quiz(
+                queestion = "PYTHON에서 무한 반복을 할 때 주로 사용하는 함수는?",
+                choices = ["for", "while True", "if", "try"],
+                answer = 2
+            ),
+            Quiz(
+                queestion = "클래스 내부에서 자기 자신을 가리키는 키워드는?",
+                choices = ["this", "self", "me", "super"],
+                answer = 2
+            )
+        ]
+        return quizzes
